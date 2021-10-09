0949a8827ad55d05f7487aedb8be83ae90c384b6
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-09 12:58:39 +0200

    implements Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,18 @@
-const Home = () => <h1>Home</h1>
+import image from '../monster-leaf.jpg'
+
+const Home = () => (
+  <main>
+    <img
+      src={image}
+      alt='Monster leaves'
+      className='absolute object-cover w-full h-full'
+    />
+    <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+      <h1 className='text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name'>
+        Aloha. I'm Lukasz!
+      </h1>
+    </section>
+  </main>
+)
 
 export default Home
